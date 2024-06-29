09e81370ab6f550afc8a8d87d9c92777cf6cec0f
Olzeev <olzeevmax@gmail.com>
2024-06-30 02:16:33 +0300

    User authentication added

--- RedCatalog/main/static/main/js/main.js
+++ RedCatalog/main/static/main/js/main.js
@@ -6,3 +6,45 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scroll');
     }
 })
+
+function login(){
+    login_window = document.getElementById('login-window')
+    login_window.style.visibility = "visible"
+    login_window.style.left = "0px"
+    screen = document.getElementById('screen')
+    screen.style.visibility = "visible"
+    document.body.style.overflow = 'hidden';
+}
+
+function close_everything(){
+    login_window = document.getElementById('login-window')
+    if (login_window.style.visibility == "visible"){
+        login_window.style.left = "calc(0px - max(400px, 25%))"
+        login_window.style.visibility = "hidden"
+    }
+
+    screen = document.getElementById('screen')
+    screen.style.visibility = "hidden"
+    document.body.style.overflow = 'auto';
+
+}
+
+function login1(){
+    button = document.getElementById('login-button')
+    button.innerHTML = '<div class="loader"></div>'
+    setTimeout(function() {
+        $.ajax({
+            url: 'login',
+            type: 'get',
+            data: {
+                mail: document.getElementById('input-mail').value,
+                password: document.getElementById('input-password').value,
+                csrfmiddlewaretoken: '{{ csrf_token }}'
+            },
+            success: function(response){
+                location.reload()
+            }
+        })
+    }, 3000);
+
+}
--- RedCatalog/main/views.py
+++ RedCatalog/main/views.py
@@ -1,7 +1,30 @@
 
 from django.shortcuts import render, redirect
-
+import django.contrib.auth as auth
+from django.views.generic import View
+from django.http import JsonResponse
 
 def index(request):
-
+    print(request)
     return render(request, 'main/index.html', {"city": 'Москва'})
+
+def registration(request):
+    mail = request.POST.get('mail')
+    password = request.POST.get('password')
+    user = auth.authenticate(email=mail, password=password)
+    if user is not None:
+
+        return render(request, 'main/registration.html', {"city": 'Москва'})
+
+
+class LoginView(View):
+    def get(self, request):
+        mail = request.GET.get('mail')
+        password = request.GET.get('password')
+
+        user = auth.authenticate(request, username=mail, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return JsonResponse({'error': 0}, status=200)
+
+        return JsonResponse({'error': 1}, status=200)
